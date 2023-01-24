622370217fd320609ccbcb8c44161cb7aa1ea056
Masaki Iida <masaki.iida@nulab.com>
2023-01-24 10:49:57 +0900

    initial commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+require('craftzdog.base')
+require('craftzdog.highlights')
+require('craftzdog.maps')
+require('craftzdog.plugins')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('craftzdog.macos')
+end
+if is_win then
+  require('craftzdog.windows')
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "plugins.lua" },
+  command = "PackerCompile",
+})
